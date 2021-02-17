969553914f99b120106c900f41052c75d09f76bb
Wesley Oliveira <32249497+Wesley-Oliveira@users.noreply.github.com>
2021-02-17 13:34:15 -0300

    Implementing Service

---
+++ src/Api.Domain/Interfaces/Services/User/IUserService.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+using Api.Domain.Entities;
+
+namespace Api.Domain.Interfaces.Services.User
+{
+    public interface IUserService
+    {
+        Task<UserEntity> Get(Guid id);
+        Task<IEnumerable<UserEntity>> GetAll();
+        Task<UserEntity> Post(UserEntity user);
+        Task<UserEntity> Put(UserEntity user);
+        Task<bool> Delete(Guid id);
+
+        //outros recursos que fossem necessários à classe
+    }
+}
