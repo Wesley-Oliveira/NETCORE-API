f1a05e56076795e122a36c5c70ad79a94d181935
Wesley Oliveira <32249497+Wesley-Oliveira@users.noreply.github.com>
2021-02-23 11:15:14 -0300

    Refactoring UserController and Startup

--- src/Api.Application/Controllers/UsersController.cs
+++ src/Api.Application/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using Api.Domain.Interfaces.Services.User;
 using Microsoft.AspNetCore.Mvc;
 using Api.Domain.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Api.Domain.Dtos.User;
 
 namespace Api.Application.Controllers
 {
@@ -60,7 +61,7 @@ namespace Api.Application.Controllers
 
         [Authorize("Bearer")]
         [HttpPost]
-        public async Task<ActionResult> Post([FromBody] UserEntity user)
+        public async Task<ActionResult> Post([FromBody] UserDto user)
         {
             if (!ModelState.IsValid)
             {
@@ -88,7 +89,7 @@ namespace Api.Application.Controllers
 
         [Authorize("Bearer")]
         [HttpPut]
-        public async Task<ActionResult> Put([FromBody] UserEntity user)
+        public async Task<ActionResult> Put([FromBody] UserDto user)
         {
             if (!ModelState.IsValid)
             {
