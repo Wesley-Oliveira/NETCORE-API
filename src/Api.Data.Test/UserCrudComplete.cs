9da67a806fc4665807621e413454c795dc1d1e0b
Wesley Oliveira <32249497+Wesley-Oliveira@users.noreply.github.com>
2021-02-24 14:50:01 -0300

    Update User CRUD Test

--- src/Api.Data.Test/UserCrudComplete.cs
+++ src/Api.Data.Test/UserCrudComplete.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Api.Data.Context;
 using Api.Data.Implementations;
@@ -26,15 +27,41 @@ namespace Api.Data.Test
                 UserImplementation _repository = new UserImplementation(context);
                 UserEntity _entity = new UserEntity
                 {
-                    Email = "teste@mail.com",
-                    Name = "Teste"
+                    Email = Faker.Internet.Email(),
+                    Name = Faker.Name.FullName()
                 };
 
                 var _registerCreated = await _repository.InsertAsync(_entity);
                 Assert.NotNull(_registerCreated);
-                Assert.Equal("teste@mail.com", _registerCreated.Email);
-                Assert.Equal("Teste", _registerCreated.Name);
+                Assert.Equal(_entity.Email, _registerCreated.Email);
+                Assert.Equal(_entity.Name, _registerCreated.Name);
                 Assert.False(_registerCreated.Id == Guid.Empty);
+
+                _entity.Name = Faker.Name.First();
+                var _registerUpdated = await _repository.UpdateAsync(_entity);
+                Assert.NotNull(_registerUpdated);
+                Assert.Equal(_entity.Email, _registerUpdated.Email);
+                Assert.Equal(_entity.Name, _registerUpdated.Name);
+
+                var _registerExists = await _repository.ExistAsync(_registerUpdated.Id);
+                Assert.True(_registerExists);
+
+                var _registerSelected = await _repository.SelectAsync(_registerUpdated.Id);
+                Assert.NotNull(_registerSelected);
+                Assert.Equal(_registerUpdated.Email, _registerSelected.Email);
+                Assert.Equal(_registerUpdated.Name, _registerSelected.Name);
+
+                var _allRegisters = await _repository.SelectAsync();
+                Assert.NotNull(_allRegisters);
+                Assert.True(_allRegisters.Count() > 1);
+
+                var _removed = await _repository.DeleteAsync(_registerSelected.Id);
+                Assert.True(_removed);
+
+                var _defaultAdminUser = await _repository.FindByLogin("admin@mail.com");
+                Assert.NotNull(_defaultAdminUser);
+                Assert.Equal("admin@mail.com", _defaultAdminUser.Email);
+                Assert.Equal("Admin", _defaultAdminUser.Name);
             }
         }
     }
