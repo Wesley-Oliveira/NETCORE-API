7b741c7a36c3030e26de584deecc86b88ff094da
Wesley Oliveira <32249497+Wesley-Oliveira@users.noreply.github.com>
2021-02-25 10:48:21 -0300

    Adding Delete Method Test

---
+++ src/Api.Service.Test/User/DeleteTest.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using Api.Domain.Interfaces.Services.User;
+using Moq;
+using Xunit;
+
+namespace Api.Service.Test.User
+{
+    public class DeleteTest : UserTests
+    {
+        private IUserService _service;
+        private Mock<IUserService> _serviceMock;
+
+        [Fact(DisplayName = "Its possible execute the DELETE user method.")]
+        public async Task TestDeleteMethod()
+        {
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(m => m.Delete(UserId))
+                                     .ReturnsAsync(true);
+            _service = _serviceMock.Object;
+
+            var result = await _service.Delete(UserId);
+            Assert.True(result);
+
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(m => m.Delete(It.IsAny<Guid>()))
+                                     .ReturnsAsync(false);
+            _service = _serviceMock.Object;
+
+            result = await _service.Delete(Guid.NewGuid());
+            Assert.False(result);
+        }
+    }
+}
