beddf001d7c68a11a33eebeadb70db72416d2a4f
Wesley Oliveira <32249497+Wesley-Oliveira@users.noreply.github.com>
2021-02-25 16:29:06 -0300

    Adding UsersController Post created and bad request test

---
+++ src/Api.Application.Test/User/Post/BadRequestTest.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Threading.Tasks;
+using Api.Application.Controllers;
+using Api.Domain.Dtos.User;
+using Api.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Api.Application.Test.User.Post
+{
+    public class BadRequestTest
+    {
+        private UsersController _controller;
+        [Fact(DisplayName = "Its not possible execute the POST user method.")]
+        public async Task TestPost()
+        {
+            var serviceMock = new Mock<IUserService>();
+            var name = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMock.Setup(m => m.Post(It.IsAny<UserDtoCreate>())).ReturnsAsync(
+                new UserDtoCreateResult
+                {
+                    Id = Guid.NewGuid(),
+                    Name = name,
+                    Email = email,
+                    CreateAt = DateTime.UtcNow
+                }
+            );
+
+            _controller = new UsersController(serviceMock.Object);
+            _controller.ModelState.AddModelError("Name", "Is required");
+
+            Mock<IUrlHelper> url = new Mock<IUrlHelper>();
+            url.Setup(x => x.Link(It.IsAny<string>(), It.IsAny<object>())).Returns("http://localhost:5000");
+            _controller.Url = url.Object;
+
+            var UserDtoCreate = new UserDtoCreate
+            {
+                Name = name,
+                Email = email
+            };
+
+            var result = await _controller.Post(UserDtoCreate);
+            Assert.True(result is BadRequestObjectResult);
+        }
+    }
+}
---
+++ src/Api.Application.Test/User/Post/CreatedOKTest.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Threading.Tasks;
+using Api.Application.Controllers;
+using Api.Domain.Dtos.User;
+using Api.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Api.Application.Test.User.Post
+{
+    public class CreatedOKTest
+    {
+        private UsersController _controller;
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        [Fact(DisplayName = "Its possible execute the POST user method.")]
+        public async Task TestPost()
+        {
+            var serviceMock = new Mock<IUserService>();
+            var name = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMock.Setup(m => m.Post(It.IsAny<UserDtoCreate>())).ReturnsAsync(
+                new UserDtoCreateResult
+                {
+                    Id = Guid.NewGuid(),
+                    Name = name,
+                    Email = email,
+                    CreateAt = DateTime.UtcNow
+                }
+            );
+
+            _controller = new UsersController(serviceMock.Object);
+
+            Mock<IUrlHelper> url = new Mock<IUrlHelper>();
+            url.Setup(x => x.Link(It.IsAny<string>(), It.IsAny<object>())).Returns("http://localhost:5000");
+            _controller.Url = url.Object;
+
+            var UserDtoCreate = new UserDtoCreate
+            {
+                Name = name,
+                Email = email
+            };
+
+            var result = await _controller.Post(UserDtoCreate);
+            Assert.True(result is CreatedResult);
+
+            var resultValue = ((CreatedResult)result).Value as UserDtoCreateResult;
+            Assert.NotNull(resultValue);
+            Assert.Equal(UserDtoCreate.Name, resultValue.Name);
+            Assert.Equal(UserDtoCreate.Email, resultValue.Email);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
