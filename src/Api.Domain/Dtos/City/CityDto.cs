39b81969b5ff8072e95c84d7e8bb3a38a2f57bf1
Wesley Oliveira <32249497+Wesley-Oliveira@users.noreply.github.com>
2021-03-02 17:05:27 -0300

    Create CityDto

---
+++ src/Api.Domain/Dtos/City/CityDto.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Api.Domain.Dtos.City
+{
+    public class CityDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int CodIBGE { get; set; }
+        public Guid UfId { get; set; }
+    }
